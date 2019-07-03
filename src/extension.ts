1da7d3e4b2be724917f738701cba5268c43d1cb1
Forrest Cahoon <forrest.cahoon@gmail.com>
2019-07-02 22:56:50 -0400

    Refactor: don't use src subdirs

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-import ChuckSyntaxCheckProvider from './features/chuckSyntaxCheckProvider';
+import ChuckSyntaxCheckProvider from './chuckSyntaxCheckProvider';
 
 export function activate(context: vscode.ExtensionContext) {
     let config = vscode.workspace.getConfiguration("chuck");
