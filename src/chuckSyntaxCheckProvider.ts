e872668c067c0ba78c874b2ae83256550df5d87b
Forrest Cahoon <forrest.cahoon@gmail.com>
2019-09-08 21:57:29 -0500

    Add switch to extend error region

--- src/chuckSyntaxCheckProvider.ts
+++ src/chuckSyntaxCheckProvider.ts
@@ -9,24 +9,25 @@ export default class ChuckSyntaxCheckProvider {
 
     private diagnosticCollection!: vscode.DiagnosticCollection;
     private document!: vscode.TextDocument;
+    private config!: vscode.WorkspaceConfiguration;
 
     private chuckEMErrorRegex = /^\[([^\]]+)\]:line\((\d+)\)\.char\((\d+)\):\s(.*)$/;
     private chuckEMError2Regex = /^\[([^\]]+)\]:line\((\d+)\):\s(.*)$/;
 
     private doChuckSyntaxCheck(textDocument: vscode.TextDocument) {
         console.log("entering syntax check code");
-        this.document = textDocument;
-        if (this.document.languageId !== 'chuck') {
+        if (textDocument.languageId !== 'chuck') {
             return;
         }
+        this.document = textDocument;
+        this.config = vscode.workspace.getConfiguration("chuck");
         const diagnostics: vscode.Diagnostic[] = [];
         let errBuf = '';
 
-        const config = vscode.workspace.getConfiguration("chuck");
-        const command = config.get("executable", "chuck");
+        const command = this.config.get("executable", "chuck");
         // We need to clone the args array because if we don't, when we push the filename on, it
         // will actually go into the config in memory, and be in the args of our next syntax check.
-        const args: string[] = [...config.get("syntaxCheckArgs", [])];
+        const args: string[] = [...this.config.get("syntaxCheckArgs", [])];
         args.push(textDocument.fileName);
         const options = { cwd: path.dirname(this.document.fileName) };
 
@@ -59,10 +60,12 @@ export default class ChuckSyntaxCheckProvider {
                 console.log("Syntax error for a different file");
                 return;
             }
-            const line = +match[2] - 1;
-            const char = +match[3] - 1;
+            const lineNum = +match[2] - 1;
+            const charNum = +match[3] - 1;
             const message = match[4];
-            const range = new vscode.Range(new vscode.Position(line, char), new vscode.Position(line, char+1));
+            const endPosition = new vscode.Position(lineNum, charNum+1);
+            const startPosition = this.getErrorRangeStartPosition(new vscode.Position(lineNum, charNum));
+            const range = new vscode.Range(startPosition, endPosition);
             return new vscode.Diagnostic(range, message);
         }
         match = this.chuckEMError2Regex.exec(errLine);
@@ -73,13 +76,68 @@ export default class ChuckSyntaxCheckProvider {
                 console.log("Syntax error for a different file");
                 return;
             }
-            const line = +match[2] - 1;
+            const lineNum = +match[2] - 1;
             const message = match[3];
-            const range = this.document.lineAt(line).range;
+            const line = this.document.lineAt(lineNum)
+            const endPosition = line.range.end;
+            const startPosition = this.getErrorRangeStartPosition(line.range.start);
+            const range = new vscode.Range(startPosition, endPosition);
             return new vscode.Diagnostic(range, message);
         }
         return;
     }
+
+    private getErrorRangeStartPosition(reportedStartPosition: vscode.Position): vscode.Position {
+        if (!this.config.get("extendErrorRegion")) {
+            return reportedStartPosition;
+        }
+        // Find the first ";" or "}" before reportedStartPosition, and return
+        // the posiition of the first non-whitespace character found after.
+
+        // We don't try to parse out comments in this code, which means we might
+        // find an end-of-block in a comment (especially commented-out code).
+        // I didn't think it was worth the effort to avoid this. If you want to
+        // fix it, please submit a PR.
+
+        const statementEndRegex = /[;\}]/;
+        const nonWhitespaceRegex = /\S/;
+        let statementEndFound = false;
+        let lineNum = reportedStartPosition.line;
+        let charNum = reportedStartPosition.character;
+        let lineText = this.document.lineAt(lineNum).text;
+        console.log("Examining line: " + lineText);
+
+        lineText = lineText.substring(0, charNum);
+        console.log("Examining line text: " + lineText);
+        let startPosition = reportedStartPosition;
+        
+        while (!statementEndFound) {
+            let match = statementEndRegex.exec(lineText);
+            if (match !== null) {
+                statementEndFound = true;
+                let lineOffset = match.index+1;
+                lineText = lineText.substring(lineOffset);
+                match = nonWhitespaceRegex.exec(lineText);
+                if (match !== null) {
+                    startPosition = new vscode.Position(lineNum, lineOffset + match.index);
+                }
+            } else {
+                match = nonWhitespaceRegex.exec(lineText);
+                if (match !== null) {
+                    startPosition = new vscode.Position(lineNum, match.index);
+                }
+            }
+            if (lineNum == 0) {
+                statementEndFound = true;
+            }
+            if (!statementEndFound) {
+                lineNum--;
+                lineText = this.document.lineAt(lineNum).text;
+            }
+        }
+        return startPosition;
+    }
+
 
     public activate(subscriptions: vscode.Disposable[]) {
         subscriptions.push(this);
